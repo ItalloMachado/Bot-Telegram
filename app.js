5639747230ab3bfca161e673464d0e9fe59ab36a
ItalloMachado <42552075+ItalloMachado@users.noreply.github.com>
2019-04-04 02:41:17 -0300

    Update app.js
    Criando as opções do chat!

--- app.js
+++ app.js
@@ -4,14 +4,30 @@ const bot = new TelegramBot(TAKEN,{ polling:true });
 bot.on(['/start', '/hello'], (msg) => {bot.sendMessage('Welcome!')});
 bot.on('message',(msg)=>{
     console.log(msg);
+    if(msg.text.toString() === '/start'){
     bot.sendMessage(msg.chat.id,'Olá '+ msg.from.first_name+', bem vindo!!',{
         "reply_markup": {
-            "keyboard":[["linkedin"],["site pessoal"],["Telefone"]]}
+            "keyboard":[["linkedin"],["site pessoal"],["Github"]]}
     });
+    }else if(msg.text.toString() === 'linkedin'){
+        bot.sendMessage(msg.chat.id,'Linkedin: https://www.linkedin.com/in/itallo-machado-70902865/',{
+            "reply_markup": {
+                "keyboard":[["linkedin"],["site pessoal"],["Github"]]}
+        });
+    }else if(msg.text.toString() === 'site pessoal'){
+        bot.sendMessage(msg.chat.id,'Site pessoal: https://itallomachado.github.io/ ',{
+            "reply_markup": {
+                "keyboard":[["linkedin"],["site pessoal"],["Github"]]}
+        });
+    }else if(msg.text.toString() === 'Github'){
+        bot.sendMessage(msg.chat.id,'Github: https://github.com/ItalloMachado',{
+            "reply_markup": {
+                "keyboard":[["linkedin"],["site pessoal"],["Github"]]}
+        });
+    }else{
+        bot.sendMessage(msg.chat.id,'Você digitou: '+ msg.from.text,{
+            "reply_markup": {
+                "keyboard":[["linkedin"],["site pessoal"],["Github"]]}
+        });
+    }
 });
-
-// bot.sendMessage(msg.chat.id, "Welcome", {
-//     "reply_markup": {
-//         "keyboard": [["Sample text", "Second sample"],   ["Keyboard"], ["I'm robot"]]
-//         }
-//     });
